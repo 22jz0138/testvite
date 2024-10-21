00a7781616ee3c062ab1781dec5b69c8a6db1ed3
22jz0138 <22jz0138@jec.ac.jp>
2024-10-21 11:48:17 +0900

    パスに対応する日本語表示の機能作成

--- src/hooks/useSetUrlPath.js
+++ src/hooks/useSetUrlPath.js
@@ -3,10 +3,21 @@ import { useLocation } from 'react-router-dom';
 const useSetUrlPath = () => {
     const location = useLocation();
     const myPath = location.pathname;
-  // パスの先頭1文字を削除 
+    // パスの先頭1文字を削除 
     const modPath = myPath.length > 1 ? myPath.slice(1) : myPath;
-  // falsyでなければ、" ＞ " を追加
-    const finalPath = modPath ? ` > ${modPath}` : modPath;
+    const pathNames = {
+        area: '会場',
+        visitor: '来場者一覧',
+        team: 'チーム一覧',
+        reception: '受付',
+        question: 'アンケート',
+    };
+
+    // パスが指定されていれば対応する名称を取得、そうでなければパスそのまま
+    const translatedPath = pathNames[modPath] || modPath;
+
+    const finalPath = translatedPath ? ` > ${translatedPath}` : translatedPath;
+
     return finalPath;
 };
 
