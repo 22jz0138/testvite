2042a3aed5f4329e0c04e83f6926d1c2534491ec
22jz0138 <22jz0138@jec.ac.jp>
2024-12-06 14:40:29 +0900

    学生一覧画面css及びクラス名変更

--- src/components/common/student/Student.jsx
+++ src/components/common/student/Student.jsx
@@ -47,37 +47,39 @@ export default function BasicExampleDataGrid() {
     return (
         <>
             <div className={styles.listArea}>
-                <div>
+                <div className={styles.processingArea}>
                     <button className={styles.addeStudentButton} onClick={ShowModal}>登録</button>
                 </div>
                 {showModal && <StudentAddModal showFlag={showModal} setShowModal={setShowModal} selectData={teamData}/>}
-                <div className={styles.listHeader}>
-                    <ul className={styles.listTitle}>
-                        <li>ID</li>
-                        <li>学年</li>
-                        <li>チーム番号</li>
-                        <li>氏名</li>
-                        <li>学籍番号</li>
-                        <li>内定先ID</li>
-                    </ul>
-                </div>
-                <div className={styles.columnBody}>
-                    {studentData.map((item) => (
-                    <Link
-                    to={`/student/${item.id}`}
-                    >
-                        <ul key={item.number} className={styles.studentsColumnWrapper}>
-                            <div key={item.name} data-key={item.id} className={styles.studentsColumn} onClick={handleDetailClick}>
-                                <li>{item.id}</li>
-                                <li>{item.grade}</li>
-                                <li>{item.team_id}</li>
-                                <li>{item.name}</li>
-                                <li>{item.number}</li>
-                                <li>{item.employment_target_id}</li>
-                            </div>
+                <div className={styles.studentListArea}>
+                    <div className={styles.listHeader}>
+                        <ul className={styles.listTitle}>
+                            <li>ID</li>
+                            <li>学年</li>
+                            <li>チーム番号</li>
+                            <li>氏名</li>
+                            <li>学籍番号</li>
+                            <li>内定先ID</li>
                         </ul>
-                    </Link>
-                    ))}
+                    </div>
+                    <div className={styles.columnBody}>
+                        {studentData.map((item) => (
+                        <Link
+                        to={`/student/${item.id}`}
+                        >
+                            <ul key={item.number} className={styles.studentsColumnWrapper}>
+                                <div key={item.name} data-key={item.id} className={styles.studentsColumn} onClick={handleDetailClick}>
+                                    <li>{item.id}</li>
+                                    <li>{item.grade}</li>
+                                    <li>{item.team_id}</li>
+                                    <li>{item.name}</li>
+                                    <li>{item.number}</li>
+                                    <li>{item.employment_target_id}</li>
+                                </div>
+                            </ul>
+                        </Link>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
