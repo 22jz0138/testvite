61d30f65649b72860d2a634495376b9816cb1e49
ninomiya0811 <yoshi0811fumi@gmail.com>
2024-12-29 18:37:54 +0900

    reacticons追加・ヘッダーおよび、サイドバーデザイン変更

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IoMdLogIn } from "react-icons/io";
+
 import { useState } from 'react';
 
 export default function Header({ toggleSidebar, path  }) {
   return (
     <>
       <header className={styles.Header}>
-        <FontAwesomeIcon icon="fa-solid fa-bars" size='lg' className={styles.ham}  onClick={toggleSidebar}/>
-        <h1 className={styles.kadwabold}>JPages</h1>
-        <h1>{path}</h1>
+        <div className={styles.logosArea}> 
+          <FontAwesomeIcon icon="fa-solid fa-bars" size='lg' className={styles.ham}  onClick={toggleSidebar}/>
+          <h1 className={styles.kadwabold}>JPages</h1>
+        </div>
+        {/* <h1>{path}</h1> */}
+        <div className={styles.loginArea}>
+          <IoMdLogIn size="3em"/>
+        </div>
       </header>
     </>
-    
-    
   )
 }
--- src/components/common/sidebar/RoutingSidebar.jsx
+++ src/components/common/sidebar/RoutingSidebar.jsx
@@ -1,51 +1,147 @@
-import React from 'react'
-import { Routes, Route,Link, NavLink } from 'react-router-dom';
+// import React from 'react'
+// import { Routes, Route,Link, NavLink } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faPeopleGroup ,faPersonShelter,faQrcode,faClipboardQuestion,faPersonChalkboard,faGraduationCap,faChartLine} from '@fortawesome/free-solid-svg-icons';  // 必要なアイコンをインポート
+// import styles from './RoutingSidebar.module.css';
+
+// export default function RoutingSidebar({ checkbool}) {
+// return (
+//     <>  
+//         <aside className={styles.sideBerArea}>
+//             <ul className={`${styles.list} ${checkbool ? '' : styles.close}`}>
+//                 <li>
+//                     <Link to="/admin/">
+//                         {checkbool?<p>アナリティクス</p>: <FontAwesomeIcon icon={faChartLine} /> }
+//                     </Link>
+//                 </li>
+//                 <li>
+//                     <Link to="/admin/team">
+//                         {checkbool?<p>チーム一覧</p>: <FontAwesomeIcon icon={faPeopleGroup} /> }
+//                     </Link>
+//                 </li>
+//                 <li>
+//                     <Link to="/admin/visitor">
+//                         {checkbool?<p>来場者</p>: <FontAwesomeIcon icon={faPersonShelter}/>}
+//                     </Link>
+//                 </li>
+//                 <li>
+//                     <Link to="/admin/student">
+//                         {checkbool?<p>学生</p>: <FontAwesomeIcon icon={faGraduationCap} />}
+//                     </Link>
+//                 </li>
+//                 <li>
+//                     <Link to="/admin/question">
+//                         {checkbool?<p>アンケート</p>: <FontAwesomeIcon icon={faClipboardQuestion}/>}
+//                     </Link>
+//                 </li>
+//                 <li>
+//                     <Link to="/admin/area">
+//                         {checkbool?<p>会場</p>:<FontAwesomeIcon icon={faPersonChalkboard}/>}
+//                     </Link>
+//                 </li>
+//             </ul>
+//         </aside>
+//     </>
+// )
+// }
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPeopleGroup ,faPersonShelter,faQrcode,faClipboardQuestion,faPersonChalkboard,faGraduationCap,faChartLine} from '@fortawesome/free-solid-svg-icons';  // 必要なアイコンをインポート
+import { faPeopleGroup, faPersonShelter, faClipboardQuestion, faPersonChalkboard, faGraduationCap, faChartLine } from '@fortawesome/free-solid-svg-icons';
 import styles from './RoutingSidebar.module.css';
 
-export default function RoutingSidebar({ checkbool}) {
-return (
-    <>  
-        <aside>
+export default function RoutingSidebar({ checkbool }) {
+    const location = useLocation();
+    const currentPath = location.pathname;
+
+    return (
+        <aside className={styles.sideBerArea}>
             <ul className={`${styles.list} ${checkbool ? '' : styles.close}`}>
                 <li>
                     <Link to="/admin/">
-                        {checkbool?'アナリティクス': <FontAwesomeIcon icon={faChartLine} /> }
+                        {checkbool ? (
+                            <div className={styles.innerList}>
+                                <FontAwesomeIcon icon={faChartLine} />
+                                <p className={`${currentPath === '/admin/' ? styles.active : ''}`}>アナリティクス</p>
+                            </div>
+                        ) : (
+                            <div className={styles.iconArea}>
+                                <FontAwesomeIcon icon={faChartLine} />
+                            </div>
+                        )}
                     </Link>
                 </li>
                 <li>
                     <Link to="/admin/team">
-                        {checkbool?'チーム一覧': <FontAwesomeIcon icon={faPeopleGroup} /> }
+                        {checkbool ? (
+                            <div className={styles.innerList}>
+                                <FontAwesomeIcon icon={faPeopleGroup} />
+                                <p className={`${currentPath === '/admin/team' ? styles.active : ''}`}>チーム一覧</p>
+                            </div>
+                        ) : (
+                            <div className={styles.iconArea}>
+                                <FontAwesomeIcon icon={faPeopleGroup} />
+                            </div>
+                        )}
                     </Link>
                 </li>
                 <li>
                     <Link to="/admin/visitor">
-                        {checkbool?'来場者': <FontAwesomeIcon icon={faPersonShelter}/>}
+                        {checkbool ? (
+                            <div className={styles.innerList}>
+                                <FontAwesomeIcon icon={faPersonShelter} />
+                                <p className={`${currentPath === '/admin/visitor' ? styles.active : ''}`}>来場者</p>
+                            </div>
+                        ) : (
+                            <div className={styles.iconArea}>
+                                <FontAwesomeIcon icon={faPersonShelter} />
+                            </div>
+                        )}
                     </Link>
                 </li>
                 <li>
                     <Link to="/admin/student">
-                        {checkbool?'学生': <FontAwesomeIcon icon={faGraduationCap} />}
+                        {checkbool ? (
+                            <div className={styles.innerList}>
+                                <FontAwesomeIcon icon={faGraduationCap} />
+                                <p className={`${currentPath === '/admin/student' ? styles.active : ''}`}>学生</p>
+                            </div>
+                        ) : (
+                            <div className={styles.iconArea}>
+                                <FontAwesomeIcon icon={faGraduationCap} />
+                            </div>
+                        )}
                     </Link>
                 </li>
                 <li>
-                    <Link to="/admin/reception">
-                        {checkbool?'受付': <FontAwesomeIcon icon={faQrcode}/>}
-                    </Link>
-                </li>                
-                <li>
                     <Link to="/admin/question">
-                        {checkbool?'アンケート': <FontAwesomeIcon icon={faClipboardQuestion}/>}
+                        {checkbool ? (
+                            <div className={styles.innerList}>
+                                <FontAwesomeIcon icon={faClipboardQuestion} />
+                                <p className={`${currentPath === '/admin/question' ? styles.active : ''}`}>アンケート</p>
+                            </div>
+                        ) : (
+                            <div className={styles.iconArea}>
+                                <FontAwesomeIcon icon={faClipboardQuestion} />
+                            </div>
+                        )}
                     </Link>
                 </li>
                 <li>
                     <Link to="/admin/area">
-                        {checkbool?'会場':<FontAwesomeIcon icon={faPersonChalkboard}/>}
+                        {checkbool ? (
+                            <div className={styles.innerList}>
+                                <FontAwesomeIcon icon={faPersonChalkboard} />
+                                <p className={`${currentPath === '/admin/area' ? styles.active : ''}`}>会場</p>
+                            </div>
+                        ) : (
+                            <div className={styles.iconArea}>
+                                <FontAwesomeIcon icon={faPersonChalkboard} />
+                            </div>
+                        )}
                     </Link>
                 </li>
             </ul>
         </aside>
-    </>
-)
+    );
 }
